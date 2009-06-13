9395cedf160f348b182043a68a4998c1b1d759f6
Daniel Woodhouse <wodemoneke@gmail.com>
2009-06-13 17:24:30 +0300

    added settings

---
+++ pms/client/settings.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+class Settings(object):
+    NAME = "pms"
+    VERSION = "0.10"
+    AUTHOR = "Daniel Woodhouse"
+    EMAIL = "wodemoneke@gmail.com"
+    WEBSITE = "http://github.com/woodenbrick/PMS"
+    WEBSITE_BUG = "http://bugreport.com"
+    LICENCE = """
+pms is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+pms is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pms.  If not, see http://www.gnu.org/licenses/
+"""
+    FACEBOOK_TIMEOUT = 300000
+    AVATAR_CHECK_TIMEOUT = 900000
+    NICETIME_TIMOUT = 60000
+    #these will be set at runtime
+    HOME = ""
+    HOMEMAIN = ""
+    IMAGES = ""
+    GLADE = ""
+    LOGGING_LEVEL = None
+    LOGO1 = ""
+    LOGO2 = ""
+    SERVER = ""
+    USERNAME = ""
