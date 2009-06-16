e8e72ec6a6f69151f2b13cb458d282834cc00ecd
Daniel Woodhouse <wodemoneke@gmail.com>
2009-06-16 20:04:39 +0300

    rearragement

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ import glob
 PROGRAM_NAME = 'pms-client'
 VERSION = '0.10'
 
-glade = glob.glob(os.path.join("pms", "client", "glade", "*.glade"))
-images = glob.glob(os.path.join("pms", "client", "images", "*"))
+glade = glob.glob(os.path.join("client", "glade", "*.glade"))
+images = glob.glob(os.path.join("client", "images", "*"))
 desc = """Desktop client for private microblogging service"""
 long_desc = """ Allows the user to keep in touch with groups of people. Groups can be password protected. Also includes Facebook status update and retrieval."""
 
@@ -21,8 +21,8 @@ setup ( name = PROGRAM_NAME,
 	    license = 'GPLv3',
         platforms = ['Linux'],
         url = 'http://github.com/woodenbrick/pms/tree',
-        packages = ['pms.client', 'pms.client.poster', 
-                    'pms.client.facebook'],
+        packages = ['client', 'client.poster', 
+                    'client.facebook'],
         data_files = [
             ('share/applications/', ['pms.desktop']),
             ('share/pms/glade', glade),
